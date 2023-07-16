f750e13540c3c34f9bc335add29f5b2552367dd2
ryu <114303361+ryuapp@users.noreply.github.com>
2023-07-16 19:00:51 +0900

    📦️ Bump up std@0.194.0

--- deps.ts
+++ deps.ts
@@ -1,3 +1,3 @@
-export { parse } from 'https://deno.land/std@0.193.0/flags/mod.ts'
-export { printf } from 'https://deno.land/std@0.193.0/fmt/printf.ts'
-export { brightBlue, brightCyan } from 'https://deno.land/std@0.193.0/fmt/colors.ts'
+export { parse } from 'https://deno.land/std@0.194.0/flags/mod.ts'
+export { printf } from 'https://deno.land/std@0.194.0/fmt/printf.ts'
+export { brightBlue, brightCyan } from 'https://deno.land/std@0.194.0/fmt/colors.ts'
