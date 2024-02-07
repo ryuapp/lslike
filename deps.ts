94640c6456e4d549eec3eba087bdef9bcd274540
ryu <114303361+ryuapp@users.noreply.github.com>
2024-02-07 23:50:08 +0900

    📦️ Bump up std@0.214.0

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export { parseArgs } from "https://deno.land/std@0.209.0/cli/mod.ts";
-export { printf } from "https://deno.land/std@0.209.0/fmt/printf.ts";
+export { parseArgs } from "https://deno.land/std@0.214.0/cli/mod.ts";
+export { printf } from "https://deno.land/std@0.214.0/fmt/printf.ts";
 export {
   brightBlue,
   brightCyan,
-} from "https://deno.land/std@0.209.0/fmt/colors.ts";
+} from "https://deno.land/std@0.214.0/fmt/colors.ts";
